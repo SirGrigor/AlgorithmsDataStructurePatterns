0fc645f7e2b781231342f534c9b411cbba12e80e
gabriel <grigorjev.ilja@gmail.com>
2022-06-14 14:45:49 +0300

    Linear

---
+++ src/algorithms/linearSearch/LinearSearch.java
@@ -0,0 +1,12 @@
+package algorithms.linearSearch;
+
+public class LinearSearch {
+    public static int search (int[] arr, int x){
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+}
